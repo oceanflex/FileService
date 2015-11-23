5c295b2c72c25978bdf1f39820c7ff594bcf1f70
Zachary <Zachary@10.0.1.5>
2015-11-23 15:45:24 -0600

    added readFile to FileReader, and tested with main method

--- src/workers/TextFileReader.java
+++ src/workers/TextFileReader.java
@@ -2,6 +2,10 @@ package workers;
 
 import format.Formatter;
 import format.GarageTotalsFormat;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Map;
 
 /**
  *
@@ -17,11 +21,40 @@ public class TextFileReader {
     }
 
     public void setFmt(Formatter fmt) {
+        //needs validation
         this.fmt = fmt;
     }
 
     public void setFilePath(String filePath) {
+        //needs validation
         this.filePath = filePath;
+    }
+    
+    public Map<String, Object>[] readFile(){
+        File data = new File(filePath);
+        BufferedReader in;
+        StringBuilder sb = new StringBuilder("");
+        try{
+            in = new BufferedReader(new FileReader(data));
+            String line = in.readLine();
+            sb = new StringBuilder(line);
+            while(line != null){
+                sb.append("\n");
+                line = in.readLine();
+                sb.append(line);
+            }
+        }catch(Exception e){
+            
+        }
+        Map<String, Object>[] back = fmt.decode(sb.toString());
+        return back;
+    }
+    
+    public static void main(String[] args) {
+        GarageTotalsFormat fmt = new GarageTotalsFormat();
+        TextFileReader read = new TextFileReader(fmt,"");
+        Map<String, Object>[] temp = read.readFile();
+        System.out.println(fmt.encode(temp));
     }
     
 }
